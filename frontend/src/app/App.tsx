ea8d7252bb23a63bfe7f5ccdf441dad424e528a0
Michal Roučka <michroucka7@gmail.com>
2025-10-06 23:05:09 +0200

    refactor(frontend): change max width of content to affect all subpages

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -6,7 +6,7 @@ export default function App() {
     return (
         <>
             <Navbar />
-            <main className="px-6 sm:px-8 md:px-12 lg:px-16 xl:px-20">
+            <main className="sm:w-3/4 2xl:w-3/5 mx-4 sm:mx-auto my-12">
                 <Routes>
                     <Route path="/" element={<Home />} />
                     {/* ... */}
