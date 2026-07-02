0ae36c5d14493f086306eb8f5b969b278ae96eb3
Michal Roučka <michroucka7@gmail.com>
2026-07-02 20:20:30 +0200

    fix demo production api error

--- backend/src/main/java/cz/zcu/kiv/caretracker/service/PictureService.java
+++ backend/src/main/java/cz/zcu/kiv/caretracker/service/PictureService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
-import java.io.InputStream;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -45,8 +44,6 @@ public class PictureService {
      */
     @Transactional
     public Picture savePicture(Long clientId, MultipartFile file) throws IOException {
-        validateFile(file);
-
         Client client = clientRepository.findById(clientId)
                 .orElseThrow(() -> new ResourceNotFoundException("Klient nenalezen"));
 
